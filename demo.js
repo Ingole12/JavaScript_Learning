49522d258c017143c50673890e029442ebb3689f
Rahul <rahulingole2112@gmail.com>
2022-10-04 19:03:40 +0530

    First  program execution in js
    First  program execution in js

---
+++ demo.js
@@ -0,0 +1,7 @@
+
+console.log("My Name is Rahul Arjun Ingole");
+console.log("My Hobbies : Swimming,Traveling, watching movies")
+console.info("Strength:Quick learning,Hard working,Honest Person")
+console.warn("Weakness:I do work one at a time ,I very emotional , I trust any one ")
+console.log(30+50);
+console.log(49*479);
