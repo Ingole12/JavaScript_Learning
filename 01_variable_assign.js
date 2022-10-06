e66fe33abe9294728885d28fc43605d5ad4f91d2
Rahul <rahulingole2112@gmail.com>
2022-10-06 17:32:08 +0530

    variables assignments
    variables assignments

---
+++ 01_variable_assign.js
@@ -0,0 +1,18 @@
+console.log("-------------Q 1-----------");
+
+var myFullName;
+console.log(myFullName);
+myFullName="Rahul Ingole";
+console.log(myFullName);
+myFullName="Rahul Arjun Ingole";
+console.log(myFullName);
+
+console.log("-------------Q 2-----------");
+
+var myLove="I love only JS";
+console.log(myLove);
+
+console.log("-------------Q 3-----------");
+
+var firstName="Rahul", lastName="Ingole", age="23" , address="A/p- Sangola" , pin_code="413307",gender="Male",is_married="Not Married";
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+var my_apple;          // variable declaration
+my_apple="Apple";           //variable initialization
+var my_name="Rahul Ingole"  //variable declaration and initialization
+console.log(my_apple);
+
+
+
+var my_number;
+my_number="100";
+console.log(my_number);
