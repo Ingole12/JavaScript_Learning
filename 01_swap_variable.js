ed3829b38cfba6f4f2c204a8186fe47af7a573fe
Rahul <rahulingole2112@gmail.com>
2022-10-07 22:29:35 +0530

    function
    function

---
+++ 01_swap_variable.js
@@ -0,0 +1,10 @@
+var no_1="100";
+var no_2="200";
+console.log("Before swap");
+console.log(no_1 , no_2);
+
+var no_x=no_1;
+no_1=no_2;
+no_2=no_x;
+console.log("After swap");
+console.log(no_1 , no_2);
